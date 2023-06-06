2a92f457e1b1c3120b6ee919a1750f52d1df0b39
Galder Zamarreño <galder@zamarreno.com>
2023-06-06 12:06:17 +0200

    Remove unused OldObject field

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/oldobject/OldObject.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/oldobject/OldObject.java
@@ -40,7 +40,6 @@ final class OldObject {
     long stackTraceId;
     long heapUsedAtLastGC;
     int arrayLength;
-    OldObject previous;
 
     @Uninterruptible(reason = "Accesses allocation sampler.")
     void set(WeakReference<?> ref, long allocatedSize, long allocatedTime, long threadId, long stackTraceId, long heapUsedAtLastGC, int arrayLength) {
@@ -62,6 +61,5 @@ final class OldObject {
         this.stackTraceId = 0L;
         this.heapUsedAtLastGC = 0L;
         this.arrayLength = 0;
-        this.previous = null;
     }
 }
