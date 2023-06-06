dff6f625d51c399d17bd9d3e3bd6f07e129ce1e3
Galder Zamarreño <galder@zamarreno.com>
2023-06-06 05:45:47 +0200

    Supress warning on unused parameter

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/SubstrateJVM.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/SubstrateJVM.java
@@ -615,7 +615,7 @@ public class SubstrateJVM {
     /**
      * See {@link JVM#emitOldObjectSamples(long, boolean, boolean)}.
      */
-    void emitOldObjectSamples(long cutoff, boolean emitAll, boolean skipBFS) {
+    void emitOldObjectSamples(long cutoff, boolean emitAll, @SuppressWarnings("unused") boolean skipBFS) {
         // todo support skipBFS=true which means using DFS (path-to-gc-roots)
         oldObjectSampler.emit(cutoff, emitAll);
     }
