bb17e4a3a186a8a1f51d5f390206d8c350f48a3c
Galder Zamarreño <galder@zamarreno.com>
2023-07-18 14:04:23 +0200

    Add support for -XX:+HeapDumpOnOutOfMemoryError

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/dump/HeapDumpSupportImpl.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/dump/HeapDumpSupportImpl.java
@@ -29,15 +29,23 @@ import static com.oracle.svm.core.heap.RestrictHeapAccess.Access.UNRESTRICTED;
 
 import java.io.IOException;
 
+import com.oracle.svm.core.SubstrateOptions;
+import com.oracle.svm.core.SubstrateUtil;
+import com.oracle.svm.core.headers.LibC;
 import org.graalvm.compiler.api.replacements.Fold;
 import org.graalvm.nativeimage.Platform;
 import org.graalvm.nativeimage.Platforms;
+import org.graalvm.nativeimage.ProcessProperties;
 import org.graalvm.nativeimage.StackValue;
+import org.graalvm.nativeimage.UnmanagedMemory;
 import org.graalvm.nativeimage.c.struct.RawField;
 import org.graalvm.nativeimage.c.struct.RawStructure;
 import org.graalvm.nativeimage.c.struct.SizeOf;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
 import org.graalvm.nativeimage.impl.HeapDumpSupport;
 import org.graalvm.word.Pointer;
+import org.graalvm.word.UnsignedWord;
 import org.graalvm.word.WordFactory;
 
 import com.oracle.svm.core.UnmanagedMemoryUtil;
@@ -54,11 +62,14 @@ import com.oracle.svm.core.thread.VMOperation;
 public class HeapDumpSupportImpl implements HeapDumpSupport {
     private final HeapDumpWriter writer;
     private final HeapDumpOperation heapDumpOperation;
+    private final HeapDumpOnErrorOperation heapDumpOnErrorOperation;
+    private CCharPointer heapOnErrorDumpPath;
 
     @Platforms(Platform.HOSTED_ONLY.class)
     public HeapDumpSupportImpl(HeapDumpMetadata metadata) {
         this.writer = new HeapDumpWriter(metadata);
         this.heapDumpOperation = new HeapDumpOperation();
+        this.heapDumpOnErrorOperation = new HeapDumpOnErrorOperation();
     }
 
     @Override
@@ -73,6 +84,32 @@ public class HeapDumpSupportImpl implements HeapDumpSupport {
         } finally {
             getFileSupport().close(fd);
         }
+    }
+
+    @Override
+    public void dumpHeapOnOutOfMemoryError() {
+        int size = SizeOf.get(HeapDumpOnErrorVMOperationData.class);
+        HeapDumpOnErrorVMOperationData data = StackValue.get(size);
+        UnmanagedMemoryUtil.fill((Pointer) data, WordFactory.unsigned(size), (byte) 0);
+
+        data.setHeapDumpPath(heapOnErrorDumpPath);
+        heapDumpOnErrorOperation.enqueue(data);
+    }
+
+    @Override
+    public void initHeapDumpOnOutOfMemoryErrorPath() {
+        String dumpFileName = "svm-heapdump-" + ProcessProperties.getProcessID() + ".hprof";
+        String dumpPath = SubstrateOptions.getHeapDumpPath(dumpFileName);
+        try (CTypeConversion.CCharPointerHolder cPath = CTypeConversion.toCString(dumpPath)) {
+            heapOnErrorDumpPath = copyDumpPath(cPath.get());
+        }
+    }
+
+    private static CCharPointer copyDumpPath(CCharPointer cPath) {
+        final UnsignedWord length = SubstrateUtil.strlen(cPath);
+        final CCharPointer copy = UnmanagedMemory.malloc(length);
+        LibC.memcpy(copy, cPath, length);
+        return copy;
     }
 
     public void writeHeapTo(RawFileDescriptor fd, boolean gcBefore) throws IOException {
@@ -115,6 +152,15 @@ public class HeapDumpSupportImpl implements HeapDumpSupport {
         void setSuccess(boolean value);
     }
 
+    @RawStructure
+    private interface HeapDumpOnErrorVMOperationData extends NativeVMOperationData {
+        @RawField
+        CCharPointer getHeapDumpPath();
+
+        @RawField
+        void setHeapDumpPath(CCharPointer value);
+    }
+
     private class HeapDumpOperation extends NativeVMOperation {
         @Platforms(Platform.HOSTED_ONLY.class)
         HeapDumpOperation() {
@@ -141,6 +187,47 @@ public class HeapDumpSupportImpl implements HeapDumpSupport {
         private void reportError(Throwable e) {
             Log.log().string("An exception occurred during heap dumping. The data in the heap dump file may be corrupt.").newline().string(e.getClass().getName()).string(": ")
                             .string(e.getMessage());
+        }
+    }
+
+    private class HeapDumpOnErrorOperation extends NativeVMOperation {
+        @Platforms(Platform.HOSTED_ONLY.class)
+        HeapDumpOnErrorOperation() {
+            super(VMOperationInfos.get(HeapDumpOnErrorOperation.class, "Write heap dump on OutOfMemoryError", SystemEffect.SAFEPOINT));
+        }
+
+        @Override
+        @RestrictHeapAccess(access = NO_ALLOCATION, reason = "Heap dumping must not allocate.")
+        protected void operate(NativeVMOperationData d) {
+            HeapDumpOnErrorVMOperationData data = (HeapDumpOnErrorVMOperationData) d;
+
+            final CCharPointer heapDumpPath = data.getHeapDumpPath();
+            final RawFileDescriptor fd = getFileSupport().create(heapDumpPath, FileCreationMode.CREATE_OR_REPLACE, RawFileOperationSupport.FileAccessMode.READ_WRITE);
+            if (!getFileSupport().isValid(fd)) {
+                Log.log().string("Invalid file descriptor opening heap dump on OutOfMemoryError.").newline();
+                return;
+            }
+
+            try {
+                final boolean success = writer.dumpHeap(fd);
+                if (!success) {
+                    reportFailure();
+                }
+            } catch (Throwable e) {
+                reportError(e);
+            } finally {
+                getFileSupport().close(fd);
+            }
+        }
+
+        @RestrictHeapAccess(access = NO_ALLOCATION, reason = "Error reporting must not allocate.")
+        private void reportFailure() {
+            Log.log().string("An error occurred while writing the heap dump on OutOfMemoryError.").newline();
+        }
+
+        @RestrictHeapAccess(access = NO_ALLOCATION, reason = "Error reporting must not allocate.")
+        private void reportError(Throwable e) {
+            Log.log().string("An exception occurred during heap dumping on OutOfMemoryError. The data in the heap dump file may be corrupt. ").string(e.getClass().getName());
         }
     }
 }
