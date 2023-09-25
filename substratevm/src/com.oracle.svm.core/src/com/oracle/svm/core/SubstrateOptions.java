ba30b3874b14c6c8863116f76a92aa2348ac4d37
Galder Zamarreño <galder@zamarreno.com>
2023-09-25 13:04:03 +0200

    Add better help for flight recorder options

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateOptions.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateOptions.java
@@ -905,7 +905,7 @@ public class SubstrateOptions {
     @Option(help = "file:doc-files/FlightRecorderLoggingHelp.txt")//
     public static final RuntimeOptionKey<String> FlightRecorderLogging = new RuntimeOptionKey<>("all=warning", Immutable);
 
-    @Option(help = "Flight Recorder options")//
+    @Option(help = "file:doc-files/FlightRecorderOptionsHelp.txt")//
     public static final RuntimeOptionKey<String> FlightRecorderOptions = new RuntimeOptionKey<>("", Immutable);
 
     public static String reportsPath() {
